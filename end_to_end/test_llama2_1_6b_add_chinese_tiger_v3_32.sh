e9bd49e8c42de5e9fda9e8a30d7f7f298ec3866c
genggui001 <genggui001@gmail.com>
2024-03-18 08:08:02 +0000

    add

--- end_to_end/test_llama2_1_6b_add_chinese_tiger_v3_32.sh
+++ end_to_end/test_llama2_1_6b_add_chinese_tiger_v3_32.sh
@@ -15,7 +15,7 @@ max_target_length=4096
 devices=32
 per_device_batch_size=4
 
-warmup_steps=2000
+warmup_steps=200
 
 forward_steps=$(($all_token / $max_target_length / $devices / $per_device_batch_size))
 gradient_accumulation_steps=$(($all_batch_token / $max_target_length / $devices / $per_device_batch_size))
