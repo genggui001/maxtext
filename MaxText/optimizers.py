1a9df7cc26e4f92d144d9907aff3aad221691156
genggui001 <genggui001@gmail.com>
2024-03-21 13:01:55 +0000

    add

--- MaxText/optimizers.py
+++ MaxText/optimizers.py
@@ -364,7 +364,7 @@ def tiger_pax(
           update_norm = optax.safe_norm(u, 0.0, ord=2, axis=(0, 2), keepdims=True)
           trust_ratio = param_norm / update_norm
 
-          trust_ratio = jnp.reshape(trust_ratio, (1, old_shape[1]) + (1,) * (old_shape - 2))
+          trust_ratio = jnp.reshape(trust_ratio, (1, old_shape[1]) + (1,) * (len(old_shape) - 2))
 
           scale = jnp.where(jnp.logical_or(param_norm == 0., update_norm == 0.), jnp.array(1.0, dtype=p.dtype), trust_ratio)
         else:
