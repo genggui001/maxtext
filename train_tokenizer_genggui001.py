d3402d6b9550b79b39143de783977dae099d4445
genggui001 <genggui001@gmail.com>
2024-04-12 10:00:44 +0000

    add

--- train_tokenizer_genggui001.py
+++ train_tokenizer_genggui001.py
@@ -3,7 +3,7 @@ import glob
 import sentencepiece as spm
 from tqdm.auto import tqdm
 
-input_sentence_size = 40000000
+input_sentence_size = 35000000
 
 def data_loader():
 
@@ -21,54 +21,70 @@ def data_loader():
 
     random.seed(42)
 
-    # zh_data_paths = sorted(
-    #     glob.glob("/home/genggui001/gdrive/gg-nlp-lm-new/gg_en/**/*.jsonl.gz", recursive=True)
-    # )
-    # random.shuffle(zh_data_paths)
-
-    zh_data_paths = [
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_en/SlimPajama-627B/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_en/WebText-en/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_en/falcon-refinedweb/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_en/the_pile_deduplicated/chunk-00000.jsonl.gz",
-
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/CCI-Data/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/SkyPile-150B/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/TeleChat-PTD/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/WebText-cn/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/WuDaoCorpus2.0/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/wangan/chunk-00000.jsonl.gz",
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_zh/yayi2_pretrain_data/chunk-00000.jsonl.gz",
-
-        "/home/genggui001/gdrive/gg-nlp-lm-new/the-stack-dedup/shuffle_data/schunk-00000.jsonl.gz",
-
-        "/home/genggui001/gdrive/gg-nlp-lm-new/gg_others_shuffle/s/schunk-00000.jsonl.gz",
-    ]
+    zh_data_paths = sorted(
+        tf.io.gfile.glob("/home/genggui001/gdrive/gg-nlp-lm-new-3/uonlp_culturax_shuffle/**/*.jsonl.gz")
+    )
+    random.shuffle(zh_data_paths)
+    print(zh_data_paths[:5])
 
     ds = tf.data.TextLineDataset(
         zh_data_paths,
         compression_type="GZIP",
         buffer_size=8 * 1024 * 1024,
-        num_parallel_reads=len(zh_data_paths),
+        num_parallel_reads=AUTOTUNE,
     )
-    text_max_len = 10 * 1024 * 1024
+
     def decode(x):
         x = tfio.experimental.serialization.decode_json(x, specs=SPECS)
-
-        x["text"] = tf.strings.substr(x["text"], 0, text_max_len, unit='BYTE')
         return x['text']
 
     ds = ds.map(decode, num_parallel_calls=AUTOTUNE)
     ds = ds.take(int(input_sentence_size * 1.2))
 
-    for item in (ds.as_numpy_iterator()):
+    for item in tqdm(ds.as_numpy_iterator()):
         yield item
 
+# Loads model from URL as iterator and stores the model to BytesIO.
+model = io.BytesIO()
 
-max_text_len = -1
-for text in data_loader():
-    text_len = len(text)
-    if text_len > max_text_len:
-        max_text_len = text_len
-        print(max_text_len)
+spm.SentencePieceTrainer.train(
+    sentence_iterator=data_loader(), 
+    model_prefix='spm_model/other',
+    model_type="BPE",
+    vocab_size=150000,
+    self_test_sample_size=0,
+    character_coverage=0.99995,
+    input_sentence_size=input_sentence_size,
+    seed_sentencepiece_size=1000000,
+    shrinking_factor=0.75,
+    num_threads=80,
+    num_sub_iterations=2,
+    max_sentence_length=4192,
+    shuffle_input_sentence=True,
+    max_sentencepiece_length=16,
+    split_by_unicode_script=True,
+    split_by_whitespace=True,
+    split_by_number=True,
+    treat_whitespace_as_suffix=False,
+    split_digits=True,
+    vocabulary_output_piece_score=True,
+    hard_vocab_limit=True,
+    use_all_vocab=False,
+    byte_fallback=True,
+    required_chars="",
+    unk_id=0,
+    bos_id=1,
+    eos_id=2,
+    pad_id=-1,
+    unk_piece="<unk>",
+    bos_piece="<s>",
+    eos_piece="</s>",
+    pad_piece="<pad>",
+    train_extremely_large_corpus=True,
+    normalization_rule_name="identity",
+    remove_extra_whitespaces=False,
+)
 
+# Serialize the model as file.
+with open('./spm_model/other_0411.model', 'wb') as f:
+    f.write(model.getvalue())
